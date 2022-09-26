8d785ef23c9e19d19c29d98b94f9487565cfc023
Jörn Heinemann <joernheinemann@gmx.de>
2022-09-26 12:06:28 +0200

    use the sentc logo for register

--- client/sentc/pages/register.vue
+++ client/sentc/pages/register.vue
@@ -1,60 +1,66 @@
 <template>
-	<v-row justify="center" align="center">
-		<v-col cols="12" sm="8" md="6">
-			<v-form @submit.prevent="register">
-				<v-card>
-					<v-card-title class="headline">
-						Create new account
-					</v-card-title>
-					<v-card-text>
-						<v-text-field
-							v-model.lazy="email"
-							label="Email"
-							prepend-icon="mdi-account-circle"
-							:rules="[rules.required, rules.email]"
-						/>
+	<div>
+		<v-row justify="center" align="center" class="mt-3">
+			<v-img :src="p('Sentc.png')" max-width="200" />
+		</v-row>
 
-						<v-text-field
-							v-model="password"
-							label="Password"
-							:type="showPassword ? 'text' : 'password'"
-							prepend-icon="mdi-lock"
-							:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-							:rules="[rules.required]"
-							loading
-							@click:append="showPassword = !showPassword"
-						>
-							<template #progress>
-								<v-progress-linear
-									:color="score.color"
-									:value="score.value"
-									class="text-right"
-									absolute
-									height="7"
-								/>
-							</template>
-						</v-text-field>
+		<v-row justify="center" align="center" class="mt-3">
+			<v-col cols="12" sm="10" md="8" lg="6">
+				<v-form @submit.prevent="register">
+					<v-card>
+						<v-card-title class="headline">
+							Create new account
+						</v-card-title>
+						<v-card-text>
+							<v-text-field
+								v-model.lazy="email"
+								label="Email"
+								prepend-icon="mdi-account-circle"
+								:rules="[rules.required, rules.email]"
+							/>
 
-						<v-text-field
-							v-model="password2"
-							label="Confirm password"
-							:type="showPassword ? 'text' : 'password'"
-							prepend-icon="mdi-lock"
-							:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-							:rules="[rules.required]"
-							@click:append="showPassword = !showPassword"
-						/>
-					</v-card-text>
+							<v-text-field
+								v-model="password"
+								label="Password"
+								:type="showPassword ? 'text' : 'password'"
+								prepend-icon="mdi-lock"
+								:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+								:rules="[rules.required]"
+								loading
+								@click:append="showPassword = !showPassword"
+							>
+								<template #progress>
+									<v-progress-linear
+										:color="score.color"
+										:value="score.value"
+										class="text-right"
+										absolute
+										height="7"
+									/>
+								</template>
+							</v-text-field>
 
-					<v-card-actions>
-						<v-spacer />
+							<v-text-field
+								v-model="password2"
+								label="Confirm password"
+								:type="showPassword ? 'text' : 'password'"
+								prepend-icon="mdi-lock"
+								:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+								:rules="[rules.required]"
+								@click:append="showPassword = !showPassword"
+							/>
+						</v-card-text>
 
-						<v-btn color="success" type="submit">Register</v-btn>
-					</v-card-actions>
-				</v-card>
-			</v-form>
-		</v-col>
-	</v-row>
+						<v-card-actions>
+							<v-spacer />
+
+							<v-btn color="success" type="submit">Register</v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-form>
+			</v-col>
+		</v-row>
+	</div>
 </template>
 
 <script lang="ts">
@@ -63,6 +69,7 @@ import Component from "vue-class-component";
 import zxcvbn from "zxcvbn";
 import {register} from "../../../server_dashboard_wasm/pkg/server_dashboard_wasm_cjs";
 import {SentcError} from "~/utils/types";
+import {p} from "~/utils/utils";
 
 @Component({
 	computed: {
@@ -116,12 +123,16 @@ export default class extends Vue
 
 	private rules = {
 		required: (value) => { return !!value || "Required."; },
-		counter: (value) => { return value.length <= 20 || "Max 20 characters"; },
 		email: (value) => {
 			const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 			return pattern.test(value) || "Invalid e-mail.";
 		}
 	};
+
+	private p(item: string)
+	{
+		return p(item);
+	}
 
 	private async register()
 	{
