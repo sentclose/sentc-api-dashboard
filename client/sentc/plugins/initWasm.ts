c2d125afd7f9741c56c79a24217345d854e606f2
Jörn Heinemann <joernheinemann@gmx.de>
2022-09-26 14:36:51 +0200

    use import from different path

--- client/sentc/plugins/initWasm.ts
+++ client/sentc/plugins/initWasm.ts
@@ -1,4 +1,4 @@
-import init from "../../../server_dashboard_wasm/pkg/server_dashboard_wasm_cjs";
+import init from "server_dashboard_wasm/server_dashboard_wasm_cjs";
 
 export default async function() {
 	await init(process.env.NODE_ENV === "production" ? "/dashboard/server_dashboard_wasm_bg.wasm" : "/server_dashboard_wasm_bg.wasm");
