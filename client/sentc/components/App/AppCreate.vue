49c1f2d5052395f76a8c8c9c1c1cb6ec2d81727a
Jörn Heinemann <joernheinemann@gmx.de>
2022-09-29 09:48:00 +0200

    use a new comp for app create and edit

---
+++ client/sentc/components/App/AppCreate.vue
@@ -0,0 +1,36 @@
+<template>
+	<div style="max-width: 1300px">
+		<h1>Options</h1>
+
+		<v-expansion-panels popout>
+			<v-expansion-panel>
+				<v-expansion-panel-header expand-icon="mdi-chevron-down">
+					<span><v-icon>mdi-cog</v-icon> App options</span>
+				</v-expansion-panel-header>
+				<v-expansion-panel-content>
+					<AppOptions ref="options" />
+				</v-expansion-panel-content>
+			</v-expansion-panel>
+		</v-expansion-panels>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import AppOptions from "~/components/App/AppOptions.vue";
+import {Prop} from "nuxt-property-decorator";
+@Component({
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	components: {AppOptions}
+})
+export default class AppCreate extends Vue
+{
+	@Prop({required: true})
+	private create: boolean;
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/sentc/pages/app/create.vue
@@ -0,0 +1,22 @@
+<template>
+	<AppCreate :create="true" />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import AppCreate from "~/components/App/AppCreate.vue";
+
+@Component({
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	components: {AppCreate}
+})
+export default class extends Vue
+{
+
+}
+</script>
+
+<style scoped>
+
+</style>
